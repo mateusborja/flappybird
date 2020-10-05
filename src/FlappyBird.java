a1135c28afc60b6d5b30f3bc54897a752f075f13
mateus.borja <mateus.borja@10.1.9.46>
2020-10-05 18:29:17 -0400

    add bird on screen

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,6 +1,17 @@
 //main class the game
 
 public class FlappyBird implements Jogo {
+
+	public double ground_offset = 0;
+	public double gvx = 85;
+
+	// Bird
+	public Passaro passaro;
+
+	public FlappyBird() {
+		passaro = new Passaro(95, getLargura()-112/2 + 24/2);
+	}
+
 	public String getTitulo() {
 		return "Flappy Bird Game";
 	}
@@ -14,27 +25,35 @@ public class FlappyBird implements Jogo {
 		return 512;
 
 	}
-
-	public void tique(java.util.Set<String> teclas, double dt) {
-
-	}
-
+	
+	
 	public void tecla(String tecla) {
 
 	}
-	
 
-	//Class all drawing on screen
-	public void desenhar(Tela tela) {
-		//background
-		tela.imagem("flappy.png", 0, 0, 288, 512, 0, 0, -122);
-		tela.imagem("flappy.png", 0, 0, 288, 512, 0, 288, -122);
-		tela.imagem("flappy.png", 0, 0, 288, 512, 0, 288*2, -122);
-		
-		//Ground
-		tela.imagem("flappy.png", 292, 0, 308, 112, 0, 0, getAltura()-198);
-		tela.imagem("flappy.png", 292, 0, 308, 112, 0, 308, getAltura()-198);
-		//tela.imagem(arquivo, xa, ya, larg, alt, dir, x, y);
+	//tique function time depends
+	public void tique(java.util.Set<String> teclas, double dt) {
+
+		ground_offset += dt * gvx;
+		ground_offset = ground_offset % 308;
+
+	}
+
+	// Class all drawing on screen
+	public void desenhar(Tela t) {
+		// background
+		t.imagem("flappy.png", 0, 0, 288, 512, 0, 0, -122);
+		t.imagem("flappy.png", 0, 0, 288, 512, 0, 288, -122);
+		// tela.imagem("flappy.png", 0, 0, 288, 512, 0, 288*2, -122);
+
+		// Ground
+		t.imagem("flappy.png", 292, 0, 308, 112, 0, -ground_offset, getAltura() - 198);
+		t.imagem("flappy.png", 292, 0, 308, 112, 0, 308 - ground_offset, getAltura() - 198);
+		t.imagem("flappy.png", 292, 0, 308, 112, 0, 308 * 2 - ground_offset, getAltura() - 198);
+		// tela.imagem(arquivo, xa, ya, larg, alt, dir, x, y);
+
+		// Bird in screen
+		passaro.desenhar(t);
 
 	}
 
--- src/Passaro.java
+++ src/Passaro.java
@@ -7,6 +7,25 @@ public class Passaro {
 	public static double G = 500;
 	public static double FLAP = -300;
 	
+	// Bird position
+	public Passaro (double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	
+	//Bird fisics
+	public void atualiza(double dt) {
+		vy += G*dt;
+		y += vy*dt;
+	}
+	
+	//Bird
+	public void desenhar(Tela t) {
+		t.imagem("flappy.png", 528, 128, 34, 24, 0, x, vy);
+		//t.imagem(arquivo, xa, ya, larg, alt, dir, x, vy);
+	}
 	
 
 }
