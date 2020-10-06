239d76d37d14df3bb48864f421f27a7ae37eccf8
mateus.borja <mateus.borja@10.1.9.46>
2020-10-05 20:14:35 -0400

    update jogo class

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,9 +1,8 @@
 public interface Jogo {
-	String getTitulo();
-	int getLargura();
-	int getAltura();
-	void tique (java.util.Set<String> teclas, double dt);
-	void tecla(String tecla);
-	void desenhar(Tela tela);
-
+    String getTitulo();
+    int getLargura();
+    int getAltura();
+    void tique(java.util.Set<String> teclas, double dt);
+    void tecla(String tecla);
+    void desenhar(Tela tela);
 }
